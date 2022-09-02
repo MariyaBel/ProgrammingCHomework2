301ea854bca511215fc68dc74cdf533d1877362e
Mariya <marbel@mail.ru>
2022-09-03 00:43:07 +0300

    Added task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void InputNumber()
+{
+    Console.WriteLine("Input 3 digit number");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int a = n / 10;// находим сколько десятков
+    int b = a % 10; // находим остаток от деления а на 10 (2я цифра n)
+
+
+    Console.WriteLine(b);
+
+}
+
+InputNumber();

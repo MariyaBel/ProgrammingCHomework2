ad7c0304e175b4f8eff40f7da023ab5bb774a55d
Mariya <marbel@mail.ru>
2022-09-03 01:11:18 +0300

    Add task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Задача решена переводом числа в строку
+
+
+void SelectSecondFigure()
+{
+     Console.WriteLine("Input int number");
+    int n = Convert.ToInt32(Console.ReadLine());
+     
+     if (n<100) Console.WriteLine("There is no third figure in that number");
+     else Console.Write(Convert.ToString(n)[2]);
+     
+} 
+
+SelectSecondFigure();
